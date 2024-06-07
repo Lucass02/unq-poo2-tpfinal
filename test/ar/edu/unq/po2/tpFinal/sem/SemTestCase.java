fc129b6cb77e2bb06c2d92c8b8c22060b01e6c30
Mauricio Velazquez <mauriciovelazquez211@gmail.com>
2024-06-07 20:50:22 -0300

    Probando mockito

--- test/ar/edu/unq/po2/tpFinal/sem/SemTestCase.java
+++ test/ar/edu/unq/po2/tpFinal/sem/SemTestCase.java
@@ -1,11 +1,35 @@
 package ar.edu.unq.po2.tpFinal.sem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SemTestCase {
+	private Sem sem1;
+	private Reloj reloj;
 	
+    @BeforeEach
+    public void setUp(){
+    	sem1 =  new Sem();
+    }
+    
+    @Test
+    public void testRelojEnFranjaHoraria() {
+		reloj = mock(Reloj.class);
+		when(reloj.getHora()).thenReturn(17);
+    	assertTrue(sem1.esFranjaHoraria(reloj)); 
+    }
+    
+    @Test
+    public void testRelojFueraDeFranjaHoraria() {
+		reloj = mock(Reloj.class);
+		when(reloj.getHora()).thenReturn(21);
+    	assertFalse(sem1.esFranjaHoraria(reloj)); 
+    }
+                            
 }
+
